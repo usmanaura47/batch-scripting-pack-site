d6332887a105c255548470b325411d485ef78a90
Malik Usman Aura <usmanaura47@gmail.com>
2017-06-24 15:16:42 +0500

    Updated

--- includes/custom.js
+++ includes/custom.js
@@ -1,10 +1,1 @@
-$(document).ready(function() {  
-	$("html").niceScroll();
-});
-
-
 (function(e){e.fn.model=function(t){var n={width:"50%",height:300};t=e.extend({},n,t);var r=this.context.id,i=e("body").find("#"+r+"-model");e("body").prepend('<div class="model-overlay"></div>');i.css({width:t.width,height:t.height});i.show()}})(jQuery)
-
-$('#more-info').on('click', function () {
-  $(this).model();
-});
