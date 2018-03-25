9fd2a3ce84e7b2b58d854c0f57e844e2069a3470
usman47 <usmanaura47@gmail.com>
2018-03-25 18:34:14 +0500

    Updated

--- includes/custom.js
+++ includes/custom.js
@@ -1,17 +1,6 @@
 $(document).ready(function () {
     $("html").niceScroll();
-});
-
-$("#modal-info").modal({
-    fadeDuration: 100
-});
-
-$(document).ready(function () {
-    $('.images-class').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1
+    $("#modal-info").modal({
+        fadeDuration: 100
     });
 });
